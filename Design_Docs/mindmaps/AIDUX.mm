977f6eea1ac1dd4c74927232ce3a1049bfabf8c3
shrikanthaprabhu <shrikantha@shrikanthaprabhu.com>
2024-05-02 21:22:45 -0600

    Mind Map software and Mind Map file added by Shree

---
+++ Design_Docs/mindmaps/AIDUX.mm
@@ -0,0 +1,145 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="AIDUX" FOLDED="false" ID="ID_191153586" CREATED="1610381622013" MODIFIED="1714705611577" ICON_SIZE="36 pt" LINK="menuitem:_ExternalImageAddAction" STYLE="oval" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED">
+<font SIZE="22"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/BuzanMap.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_12905907" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_12905907" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_520072399">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_520072399"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ICON_SIZE="64 pt" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Segoe Print" SIZE="22"/>
+<edge COLOR="#ffffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" ICON_SIZE="32 px" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="18" BOLD="false" ITALIC="true"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" ICON_SIZE="28 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" ICON_SIZE="24 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" ICON_SIZE="24 px" COLOR="#111111" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="13"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" ICON_SIZE="24 px" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="12"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" ICON_SIZE="24 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" ICON_SIZE="16 px">
+<font SIZE="10"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" ICON_SIZE="16 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" ICON_SIZE="16 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" ICON_SIZE="16 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" ICON_SIZE="16 px">
+<edge STYLE="bezier"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="15" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<edge COLOR="#ffffff"/>
+<node TEXT="Product Dynamics" POSITION="bottom_or_right" ID="ID_273436263" CREATED="1610381622018" MODIFIED="1714704925439" HGAP_QUANTITY="91.2683 pt" VSHIFT_QUANTITY="-30.73171 pt">
+<edge COLOR="#007c7c"/>
+<node TEXT="Sentiment" ID="ID_588194298" CREATED="1610381622019" MODIFIED="1714705026882" HGAP_QUANTITY="15.7561 pt" VSHIFT_QUANTITY="-12.29268 pt">
+<node TEXT="Overall" ID="ID_862588309" CREATED="1714705083574" MODIFIED="1714705098600"/>
+<node TEXT="Over the time" ID="ID_1342404984" CREATED="1714705099599" MODIFIED="1714705106969"/>
+</node>
+<node TEXT="Aspectwise Performance" ID="ID_1104103844" CREATED="1610381622019" MODIFIED="1714705043867">
+<node TEXT="Performance" ID="ID_1956280117" CREATED="1714705123562" MODIFIED="1714705174994"/>
+<node TEXT="Quality Perception" POSITION="bottom_or_right" ID="ID_125084069" CREATED="1714705125821" MODIFIED="1714705136672"/>
+<node TEXT="Feature Satisfaction" POSITION="bottom_or_right" ID="ID_63669291" CREATED="1714705113933" MODIFIED="1714705121231"/>
+<node TEXT="Value For Money" ID="ID_1668151206" CREATED="1714705178297" MODIFIED="1714705185678"/>
+<node TEXT="Common Problems" ID="ID_282850626" CREATED="1714705188589" MODIFIED="1714705195466"/>
+</node>
+<node TEXT="Brand, Category, SKU Level Comparison with Competitors" ID="ID_273085331" CREATED="1714705234212" MODIFIED="1714705317248"/>
+<node TEXT="Media Research and Review" ID="ID_678170894" CREATED="1714705324962" MODIFIED="1714705338488">
+<node TEXT="Understanding Changes Ecommerce Space" ID="ID_1446982964" CREATED="1714705338494" MODIFIED="1714705365956"/>
+<node TEXT="Brand Focus Area" ID="ID_634517620" CREATED="1714705373614" MODIFIED="1714705385712"/>
+<node TEXT="Changes in Customer Behaviour" ID="ID_486134388" CREATED="1714705386259" MODIFIED="1714705470675"/>
+</node>
+</node>
+<node TEXT="Customer Outreach" POSITION="top_or_left" ID="ID_575079876" CREATED="1610381622020" MODIFIED="1714704942186" HGAP_QUANTITY="67.56098 pt" VSHIFT_QUANTITY="-42.14634 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="Reactive Outreach" ID="ID_676902637" CREATED="1714705518055" MODIFIED="1714705532531">
+<node TEXT="Discover Recurrent Themes.&#xa;Identify Patterns" ID="ID_317662625" CREATED="1610381622020" MODIFIED="1714706097633" HGAP_QUANTITY="14.87805 pt" VSHIFT_QUANTITY="-18.43903 pt"/>
+<node TEXT="Real Time Customer Engagement" ID="ID_612469081" CREATED="1714705706809" MODIFIED="1714705730031">
+<node TEXT="Sentiment Analysis" ID="ID_647753106" CREATED="1714705755136" MODIFIED="1714705763514"/>
+<node TEXT="Chat Service" ID="ID_140839588" CREATED="1714705763879" MODIFIED="1714705770761"/>
+<node TEXT="Intent Identification" ID="ID_243561855" CREATED="1714705771351" MODIFIED="1714705792797"/>
+</node>
+<node TEXT="Real Time Recommendation" ID="ID_301624546" CREATED="1714705738991" MODIFIED="1714705751377"/>
+</node>
+<node TEXT="Proactive Outreach" ID="ID_1522387997" CREATED="1610381622021" MODIFIED="1714705508984">
+<node TEXT="Address Critical Reviews" ID="ID_173438186" CREATED="1714705793201" MODIFIED="1714705799733">
+<node TEXT="Trust and Credibility" ID="ID_49106118" CREATED="1714705803527" MODIFIED="1714705827421"/>
+<node TEXT="Retention" ID="ID_1998756506" CREATED="1714705805568" MODIFIED="1714705830990">
+<node TEXT="Need based product recommendation" ID="ID_214264848" CREATED="1714705830994" MODIFIED="1714705844198"/>
+<node TEXT="Communicate Addressed Product Issues" ID="ID_814361769" CREATED="1714705850555" MODIFIED="1714705864475"/>
+</node>
+</node>
+<node TEXT="Dynamic Adverstisement" ID="ID_294369329" CREATED="1714705880322" MODIFIED="1714705900210">
+<node TEXT="Improve Brand Image" ID="ID_824755974" CREATED="1714705900213" MODIFIED="1714705913013"/>
+<node TEXT="Inform New Features" ID="ID_271875756" CREATED="1714705913408" MODIFIED="1714705921329"/>
+<node TEXT="Engagement" ID="ID_236647593" CREATED="1714705921576" MODIFIED="1714705942173">
+<node TEXT="Interesting/Compelling Stories or Quotes" ID="ID_1689022979" CREATED="1714705934741" MODIFIED="1714705987749"/>
+<node TEXT="Communicate Upgrades&#xa;Better Alternatives" ID="ID_659314931" CREATED="1714705995585" MODIFIED="1714706047702"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
